0fb5ea651d77d9511cefee0a6aa3589a029811ba
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2026-08-05 23:33:10 +0300

    trig timing

--- esp32-led-blink/src/main.cpp
+++ esp32-led-blink/src/main.cpp
@@ -11,17 +11,25 @@ typedef enum {
 #define LED2 16
 #define LED3 17
 #define TOTAL_LEDS 3
-#define STEP_DELAY 200
+#define STEP_DELAY_INIT 500
+#define STEP_DELAY_MIN 10
 
 int leds[] = {
   LOW,  LOW,  LOW,
   LED1, LED2, LED3
 };
 
+int delay_state = STEP_DELAY_INIT;
+
 ledsState_t state = NONE;
 
-ledsState_t transitions[] = {LED1_ON, LED2_ON, LED3_ON, LED2_ON, LED3_ON};
+ledsState_t transitions[] = {LED1_ON, LED2_ON, LED3_ON,
+  // LED2_ON, LED3_ON
+};
 int transitions_step = 0;
+
+unsigned long last_millis = 0;
+int dt = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -30,6 +38,13 @@ void setup() {
   for (int i=TOTAL_LEDS; i<TOTAL_LEDS*2;i++) {
     pinMode(leds[i], OUTPUT);
   }
+}
+
+void calcDelta() {
+  unsigned long ms = millis();
+  dt = ms - last_millis;
+  last_millis = ms;
+  Serial.printf("delta = %d \n", dt);
 }
 
 void handleState() {
@@ -66,8 +81,12 @@ void handleTransitions() {
 }
 
 void loop() {
+  calcDelta();
   handleTransitions();
   handleState();
 
-  delay(STEP_DELAY);
+  delay_state = STEP_DELAY_MIN + STEP_DELAY_INIT + round(STEP_DELAY_INIT * cos(millis() / (PI * 750 / 2)));
+  Serial.printf("delay = %d", delay_state);
+
+  delay(delay_state);
 }
