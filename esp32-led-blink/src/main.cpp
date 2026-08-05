7e6a13c50bd7812cfc9b8d27d1076e70d8716c7c
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2026-08-05 22:16:12 +0300

    transitions

--- esp32-led-blink/src/main.cpp
+++ esp32-led-blink/src/main.cpp
@@ -2,36 +2,72 @@
 
 typedef enum {
   NONE,
-  LED1,
-  LED2,
-  LED3
+  LED1_ON,
+  LED2_ON,
+  LED3_ON,
 } ledsState_t;
 
 #define LED1 15
 #define LED2 16
 #define LED3 17
+#define TOTAL_LEDS 3
+#define STEP_DELAY 200
 
-ledsState_t ledState = NONE;
+int leds[] = {
+  LOW,  LOW,  LOW,
+  LED1, LED2, LED3
+};
+
+ledsState_t state = NONE;
+
+ledsState_t transitions[] = {LED1_ON, LED2_ON, LED3_ON, LED2_ON, LED3_ON};
+int transitions_step = 0;
 
 void setup() {
   Serial.begin(115200);
   delay(500);
+
+  for (int i=TOTAL_LEDS; i<TOTAL_LEDS*2;i++) {
+    pinMode(leds[i], OUTPUT);
+  }
 }
 
-void loop() {
-  switch (ledState) {
+void handleState() {
+  switch (state) {
     case NONE:
+      Serial.println("STATE = NONE");
+      for (int i=0; i<TOTAL_LEDS; i++) { leds[i] = LOW; }
       break;
 
-    case LED1:
+    case LED1_ON:
+      Serial.println("STATE = LED1_ON");
+      for (int i=0; i<TOTAL_LEDS; i++) { leds[i] = i == 0 ? HIGH : LOW; }
       break;
 
-    case LED2:
+    case LED2_ON:
+      Serial.println("STATE = LED2_ON");
+      for (int i=0; i<TOTAL_LEDS; i++) { leds[i] = i == 1 ? HIGH : LOW; }
       break;
 
-    case LED3:
+    case LED3_ON:
+      Serial.println("STATE = LED3_ON");
+      for (int i=0; i<TOTAL_LEDS; i++) { leds[i] = i == 2 ? HIGH : LOW; }
       break;
   }
 
-  delay(1000);
+  for (int i=0;i<TOTAL_LEDS;i++) { digitalWrite(leds[i+TOTAL_LEDS], leds[i]); }
+}
+
+void handleTransitions() {
+  transitions_step %= sizeof(transitions) / sizeof(transitions[0]);
+  Serial.printf("transitions_step = %d \n", transitions_step);
+  state = transitions[transitions_step];
+  transitions_step += 1;
+}
+
+void loop() {
+  handleTransitions();
+  handleState();
+
+  delay(STEP_DELAY);
 }
