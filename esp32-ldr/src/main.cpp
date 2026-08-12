2df28e639c3f5494e7f7c86a4a2c4bd86d77b587
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2026-08-12 22:51:28 +0300

    table with measurements

---
+++ esp32-ldr/src/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+
+#define ADC_IN 4
+#define LOOP_DELAY_MS 100
+#define ADC_MAX 4095
+#define V_REF 3300
+
+void setup() {
+  Serial.begin(115200);
+  analogReadResolution(12);
+  delay(1000);
+
+  pinMode(ADC_IN, INPUT);
+
+  Serial.println("+-------+-----------+-----------+----------+---------+");
+  Serial.println("|   RAW | calc (mV) | meas (mV) | err (mV) | err (%) |");
+  Serial.println("+-------+-----------+-----------+----------+---------+");
+}
+
+uint16_t calcAdcMv(uint16_t adcVal) { return adcVal * V_REF / ADC_MAX; }
+
+void printVoltages(uint16_t rawAdcVal, uint16_t rawAdcValmv,
+                   uint16_t adcValMv) {
+  uint16_t mvError = abs((int)rawAdcValmv - (int)adcValMv);
+  float mvErrorPct = 0.0;
+
+  if (adcValMv > 0) {
+    mvErrorPct = mvError * 100.0f / adcValMv;
+  }
+
+  Serial.printf("| %5u | %9u | %9u | %8u | %7.2f |\n", rawAdcVal, rawAdcValmv,
+                adcValMv, mvError, mvErrorPct);
+}
+
+void loop() {
+  static uint16_t adcVal, adcValmv;
+  uint16_t newAdcVal = analogRead(ADC_IN);
+
+  if (newAdcVal != adcVal) { // At least skip same rows
+    adcValmv = analogReadMilliVolts(ADC_IN);
+    adcVal = newAdcVal;
+
+    printVoltages(adcVal, calcAdcMv(adcVal), adcValmv);
+  }
+
+  delay(LOOP_DELAY_MS);
+}
