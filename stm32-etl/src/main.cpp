a4b80af179ff82b3ed277b554fb594ade770da82
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2026-08-24 18:43:29 +0300

    2.1

---
+++ stm32-etl/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#define LED_PIN PC13
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(115200);
+    Serial.println("UART ready!\r\n");
+}
+
+void loop() {
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(500);
+    Serial.println("LED blinked!");
+}
+
