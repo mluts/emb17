186ab5941859d275039ab4337113a92efa63d206
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2026-08-26 00:45:51 +0300

    motor

--- esp32-motor-2.2/src/main.cpp
+++ esp32-motor-2.2/src/main.cpp
@@ -1,161 +1,79 @@
 #include <Arduino.h>
 
-// Convenience abstraction for running code after some time has elapsed
-class Timer {
+constexpr uint32_t ADC_MAX_V = 3100;
+constexpr unsigned long DUTY_CYCLE_PERIOD = 20;
+
+class Motor {
 private:
-  unsigned long delayMs;
-  unsigned long startMs;
-
-public:
-  Timer(unsigned long delayMs) {
-    this->delayMs = delayMs;
-    restart();
-  }
-
-  Timer() { Timer(0); }
-
-  bool isReady() { return (millis() - startMs) >= delayMs; }
-
-  void restart() { startMs = millis(); }
-};
-
-// Convenience abstraction to measure time duration between start() and end()
-class DelayMeasure {
-private:
-  unsigned long measureStartMicros, measureEndMicros;
-
-  DelayMeasure(unsigned long startMs) { this->measureStartMicros = startMs; }
-
-  DelayMeasure() {
-    this->measureStartMicros = 0;
-    this->measureEndMicros = 0;
-  }
-
-public:
-  static DelayMeasure start() { return DelayMeasure(micros()); }
-
-  static DelayMeasure empty() { return DelayMeasure(0); }
-
-  bool isComplete() { return measureStartMicros != 0 && measureEndMicros != 0; }
-  bool isEmpty() { return measureStartMicros == 0 && measureEndMicros == 0; }
-
-  void end() {
-    if (!isEmpty()) {
-      this->measureEndMicros = micros();
-    }
-  }
-
-  unsigned long getMeasure() { return measureEndMicros - measureStartMicros; }
-};
-
-// Handles signal coming from GPIO to relay
-class RelayInput {
-private:
-  static uint8_t PIN;
-
-  static bool state;
-
-public:
-  static void init(uint8_t pin, unsigned long newSwitchDelayMs) {
-    PIN = pin;
-    state = false;
-
-    pinMode(PIN, OUTPUT);
-    digitalWrite(PIN, LOW);
-  }
-
-  static bool getState() { return state; }
-
-  static void doSwitch(bool newState) {
-    state = newState;
-
-    if (state) {
-      digitalWrite(PIN, HIGH); // On true we're activating relay
-    } else {
-      digitalWrite(PIN, LOW); // On false we're disabling relay
-    }
-  }
-};
-
-uint8_t RelayInput::PIN;
-bool RelayInput::state;
-
-// Handles signal coming from relay to GPIO
-class RelayOutput {
-private:
-  static uint8_t PIN;
-  volatile static bool relayOn; // Changed in ISP
+  static uint8_t motorPin;
+  static bool on;
 
 public:
   static void init(uint8_t pin) {
-    PIN = pin;
-    relayOn = false;
-
-    pinMode(PIN, INPUT_PULLUP);
+    motorPin = pin;
+    pinMode(motorPin, OUTPUT);
+    on = false;
+    digitalWrite(motorPin, LOW);
   }
 
-  static uint8_t getPin() { return PIN; }
+  static bool isOn() { return on; }
 
-  static void interruptHandler() {
-    if (digitalRead(PIN) == HIGH) {
-      relayOn = false; // HIGH -> relay is closed
+  static void set(bool newStatus) {
+    if (newStatus) {
+      digitalWrite(motorPin, HIGH);
     } else {
-      relayOn = true; // LOW -> relay is open
+      digitalWrite(motorPin, LOW);
     }
-  }
 
-  static bool getRelayOn() { return relayOn; }
+    on = newStatus;
+  }
 };
 
-uint8_t RelayOutput::PIN;
-volatile bool RelayOutput::relayOn;
+uint8_t Motor::motorPin;
+bool Motor::on;
+
+class MotorSetting {
+private:
+  static uint8_t adcPin;
+
+public:
+  static void init(uint8_t pin) {
+    adcPin = pin;
+    pinMode(adcPin, INPUT);
+  }
+
+  static float dutyCycleRatio() {
+    return (float)analogReadMilliVolts(adcPin) / ADC_MAX_V;
+  }
+
+  static unsigned long timeOn() {
+    return round(dutyCycleRatio() * DUTY_CYCLE_PERIOD);
+  }
+
+  static unsigned long timeOff() {
+    return round((1 - dutyCycleRatio()) * DUTY_CYCLE_PERIOD);
+  }
+};
+
+uint8_t MotorSetting::adcPin;
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
   Serial.println();
-  Serial.println("ESP32 Relay Exercise");
+  Serial.println("ESP32 Motor PWM Exercise");
 
-  RelayInput::init(15, 1000); // We will activate relay from GPIO 15 (via BC547)
-
-  RelayOutput::init(16); // Relay Output GPIO and Debounce MS
-
-  attachInterrupt(RelayOutput::getPin(), RelayOutput::interruptHandler, CHANGE);
+  Motor::init(15);
+  MotorSetting::init(4);
 }
 
 void loop() {
-  static Timer relayInputTimer = Timer(1000); // Create restartable timer
-  static bool isRelayOn = RelayOutput::getRelayOn();
+  Motor::set(!Motor::isOn());
 
-  static DelayMeasure measure = DelayMeasure::empty();
-
-  static unsigned long relayTimeMax, relayTimeMin;
-
-  // NOTE: Assuming that initially `isRelayOn != RelayOutput::getRelayOn()`
-  //       And becomes `isRelayOn == RelayOutput::getRelayOn()` on
-  //       next loop() iterations
-  if (!measure.isComplete() && isRelayOn == RelayOutput::getRelayOn()) {
-    Serial.print("\n      ... measurement complete, ");
-    measure.end();
-
-    Serial.printf(
-        "\n        Relay switch time: %lu(micros)\n",
-        measure.getMeasure());
-  }
-
-  if (relayInputTimer.isReady()) {
-    Serial.println();
-    // Serial.println("{TIMER} Toggle relay input");
-    Serial.print("Timer -> Toggle Relay, ");
-    bool newRelayState = !RelayInput::getState();
-    Serial.printf("\n  ...set relay to %d, ", newRelayState ? 1 : 0);
-    isRelayOn = newRelayState;
-    RelayInput::doSwitch(newRelayState);
-
-    measure = DelayMeasure::start();
-    Serial.print("\n    ...start measure output delay, ");
-
-    relayInputTimer.restart();
+  if (Motor::isOn()) {
+    delay(MotorSetting::timeOn());
+  } else {
+    delay(MotorSetting::timeOff());
   }
 }
