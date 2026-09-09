ea11f5ffab0eacd3e171e7ad6faadfdae157283e
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2026-09-09 20:30:59 +0300

    btn and led

--- esp32-espidf-first-lesson/src/main.cpp
+++ esp32-espidf-first-lesson/src/main.cpp
@@ -8,6 +8,7 @@
 #define BUTTON_IN GPIO_NUM_15
 
 extern "C" void app_main() {
+  // Initialize LED GPIO
   gpio_config_t gpio_led_conf = {};
   gpio_led_conf.pin_bit_mask = 1ull << LED_OUT;
   gpio_led_conf.mode = GPIO_MODE_OUTPUT;
@@ -17,17 +18,35 @@ extern "C" void app_main() {
   gpio_led_conf.pull_up_en = GPIO_PULLUP_DISABLE;
   gpio_led_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
 
-  gpio_set_level(LED_OUT, 0);
-
   // disable interrupts
   gpio_led_conf.intr_type = GPIO_INTR_DISABLE;
 
+  // Initialize BTN GPIO
+  gpio_config_t gpio_btn_conf = {};
+  gpio_btn_conf.pin_bit_mask = 1ull << BUTTON_IN;
+  gpio_btn_conf.mode = GPIO_MODE_INPUT;
+  gpio_btn_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+  gpio_btn_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  gpio_btn_conf.intr_type = GPIO_INTR_DISABLE;
+
+  // Configure LED and BTN GPIO
   gpio_config(&gpio_led_conf);
+  gpio_config(&gpio_btn_conf);
+
+  gpio_set_level(LED_OUT, 0);
 
   while (1) {
-    gpio_set_level(LED_OUT, 1);
-    vTaskDelay(200 / portTICK_PERIOD_MS);
-    gpio_set_level(LED_OUT, 0);
-    vTaskDelay(200 / portTICK_PERIOD_MS);
+    int btn_state = gpio_get_level(BUTTON_IN);
+
+    if (btn_state == 1) {
+      gpio_set_level(LED_OUT, 0);
+    } else {
+      gpio_set_level(LED_OUT, 1);
+    }
+
+    // vTaskDelay(200 / portTICK_PERIOD_MS);
+    vTaskDelay(100);
+    // gpio_set_level(LED_OUT, 0);
+    // vTaskDelay(200 / portTICK_PERIOD_MS);
   }
 }
